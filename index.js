9f345a6e0ecb6b367420a4449816d38d2690834f
Dmitry Polovka <messaged@messaged.lv>
2015-03-25 14:14:29 +0200

    First implementation

--- index.js
+++ index.js
@@ -1,11 +1,48 @@
-"use strict";
-
 var MessageFormat = require("messageformat");
+var fs = require("fs");
+var path = require("path");
+var _ = require("lodash");
 
-module.exports =  {
+var fileOptions = {encoding: 'utf8'};
+
+var MessageFormatTranslator = {
 	directory: function (directory, locale) {
-		var compiled = {};
+		var body = MessageFormatTranslator.directoryBody(directory, locale);
 
-		return compiled;
+		return new (new Function(body));
+	},
+	directoryBody: function (directory, locale) {
+		var mf = new MessageFormat(locale);
+		var extension = ".json";
+		var body = "this[\"" + mf.globalName + "\"]=" + mf.functions() + ";\n";
+		_.forEach(fs.readdirSync(directory), function (item) {
+			if (path.extname(item) === extension) {
+				var fullPath = _.trimRight(directory, "/") + "/" + item;
+				var read = fs.readFileSync(fullPath, fileOptions);
+				var fileBaseName = path.basename(item, extension);
+
+				var parsedObject = JSON.parse(read);
+				if (typeof parsedObject !== "object") {
+					return;
+				}
+
+				body += "this[\"" + mf.globalName + "\"][\"" + fileBaseName + "\"] = {};\n";
+				_.forEach(parsedObject, function (value, key) {
+					if (typeof value !== "string") {
+						delete parsedObject[key];
+						return;
+					}
+
+					body += "this[\"" + mf.globalName + "\"][\"" + fileBaseName + "\"][\"" + key + "\"] = " + mf.compile(value).toString() + ";\n";
+				});
+			}
+		});
+		body += "this.getNamespace = function(namespace) { " +
+		"if( ! this[\"" + mf.globalName + "\"].hasOwnProperty(namespace)) { return false;}" +
+		"return this[\"" + mf.globalName + "\"][namespace];" +
+		"}";
+		return body;
 	}
 };
+
+module.exports = MessageFormatTranslator;
